bed1fbf795cc1d15e5f23e78b00b4ebb7a408e99
Alex <legendyt123212@gmail.com>
2026-05-27 01:48:37 +0530

    v.0.1

--- NDScript/package/runtime/context.ts
+++ NDScript/package/runtime/context.ts
@@ -2,6 +2,8 @@ export class RuntimeContext {
 
     logs: string[] = []
 
+    variables: Map<string, any> = new Map()
+
     startTime: number = Date.now()
 
     log(message: string) {
@@ -9,6 +11,21 @@ export class RuntimeContext {
         this.logs.push(message)
 
         console.log(`[NDScript] ${message}`)
+
+    }
+
+    setVariable(
+        key: string,
+        value: any
+    ) {
+
+        this.variables.set(key, value)
+
+    }
+
+    getVariable(key: string) {
+
+        return this.variables.get(key)
 
     }
 
